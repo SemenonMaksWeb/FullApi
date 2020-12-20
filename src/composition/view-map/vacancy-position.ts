45ae6d75564409ab626134b3bffd2f1bd39700b5
Павел <Pavel@mail.ru>
2020-12-20 20:38:49 +0300

    удалить базовую форму

--- src/composition/view-map/vacancy-position.ts
+++
@@ -1,7 +0,0 @@
-import { InterfaceMapView } from '@/composition/view-map/type.ts';
-
-export const VacancyPositionMapTable: InterfaceMapView[] = [
-  { id: 1, name: 'id', type: 'text', text: 'id' },
-  { id: 2, name: 'name', type: 'inputString', text: 'Названия' },
-  { id: 3, name: 'active', type: 'checkbox', text: 'Видимость' },
-];
--- src/views/vacancy-position-id.vue
+++ src/views/vacancy-position-id.vue
@@ -1,23 +1,19 @@
 <template>
   <container>
-    <base-form
-      :type-form="VacancyPositionMapTable"
-      :array="VacancyPosition"
-      :title-form="'Должность Вакансии'"
-    />
+
   </container>
 </template>
 
 <script lang="ts">
 import Container from '@/components/base/container.vue';
 import { ViewsVacancyPositionId } from '@/composition/views/vacancy-position-id.ts';
-import { VacancyPositionMapTable } from '@/composition/view-map/vacancy-position';
+
 import BaseForm from '@/components/base/view/form.vue';
 export default {
   name: 'vacancy-position',
   components: { BaseForm, Container },
   setup() {
-    return { ...ViewsVacancyPositionId(), VacancyPositionMapTable };
+    return { ...ViewsVacancyPositionId() };
   },
 };
 </script>
