c05d82284b14e1384d92b7d30de0770c8232f382
Павел <Pavel@mail.ru>
2021-01-08 14:36:47 +0300

    user-set server

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,6 +4,7 @@ import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Users } from './users.entity';
+
 const bcrypt = require('bcrypt');
 @Injectable()
 export class UsersService {
@@ -12,6 +13,11 @@ export class UsersService {
     private readonly UserRepository: Repository<Users>,
   ){}
   async create(createUserDto: CreateUserDto) {
+    const user = await this.findOneValidate(createUserDto.login, createUserDto.email);
+    const message:string[] = this.validateCheckUnique(user, createUserDto);
+    if(message.length != 0){
+      return { statusCode: 400, message, error: "Bad Request"};
+    }
     createUserDto.password =  this.HashPassword(createUserDto.password);
     const data = {
       login: createUserDto.login,
@@ -36,6 +42,11 @@ export class UsersService {
   async findOneLogin(login: string) {
     return await this.UserRepository.findOne({login:login});
   }
+  async findOneValidate(login: string, email: string){
+    return await this.UserRepository.findOne(
+     { where:[{login:login}, {email:email} ] }
+    );
+  }
 
   update(id: number, updateUserDto: UpdateUserDto) {
     return `This action updates a #${id} user`;
@@ -44,12 +55,26 @@ export class UsersService {
   remove(id: number) {
     return `This action removes a #${id} user`;
   }
+
   HashPassword(password){
     const salt = bcrypt.genSaltSync(12);
     return bcrypt.hashSync(password, salt);
   }
+
   HashAdressEmail(data){
     const salt = bcrypt.genSaltSync(12);
     return bcrypt.hashSync(data, salt);
+  }
+  validateCheckUnique(user:Users, createUserDto: CreateUserDto): string[]{
+    let message = [];
+    if(user){
+      if(user.login === createUserDto.login){
+        message.push("Данный логин уже занят");
+      }
+      if(user.email === createUserDto.email){
+        message.push("Данный email уже занят");
+      }
+    }
+    return message;
   }
 }
